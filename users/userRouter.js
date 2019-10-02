dffc596fcccd67514606862a5e384a87ab20ab8d
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-02 12:52:41 -0700

    getting a specific user works

--- users/userRouter.js
+++ users/userRouter.js
@@ -16,8 +16,16 @@ router.get("/", (req, res) => {
       res.status(500).json({ error: "Error getting users" });
     });
 });
-router.get("/:id", (req, res) => {});
-
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  User.getById(id).then(user => {
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: "User with id does not exist" });
+    }
+  });
+});
 router.get("/:id/posts", (req, res) => {});
 
 router.delete("/:id", (req, res) => {});
