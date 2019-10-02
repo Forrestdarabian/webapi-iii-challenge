9b8986beef41055bfa3997cad72077d1c3b95e59
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-02 13:09:47 -0700

    Cleaned up code significantly by implementing validateByUserId middleware function and applying it to above code

--- users/userRouter.js
+++ users/userRouter.js
@@ -16,26 +16,33 @@ router.get("/", (req, res) => {
       res.status(500).json({ error: "Error getting users" });
     });
 });
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  User.getById(id).then(user => {
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ error: "User with id does not exist" });
-    }
-  });
+router.get("/:id", validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 router.get("/:id/posts", (req, res) => {});
 
 router.delete("/:id", (req, res) => {});
 
-router.put("/:id", (req, res) => {});
-
+router.put("/:id", validateUserId, (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  User.update(id, { name }).then(updated => {
+    res.status(200).json(updated);
+  });
+});
 //custom middleware
 
-function validateUserId(req, res, next) {}
-
+function validateUserId(req, res, next) {
+  const { id } = req.params.id;
+  User.getById(id).then(user => {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({ error: "User with id does not exist" });
+    }
+  });
+}
 function validateUser(req, res, next) {}
 
 function validatePost(req, res, next) {}
